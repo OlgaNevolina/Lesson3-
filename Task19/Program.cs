296ee386cdbbc7fe582da84bb0fef1de9676d108
nevol78@mail.ru <nevol78@mail.ru>
2022-05-20 18:59:27 +0300

    Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите целое число: ");
+string number = Console.ReadLine();
+int length = number.Length;
+string txtToCheck = number.Replace(" ","");  
+
+bool IsPalindrome()
+{
+    for (int i = 0; i < length / 2; i++)
+        while (number[i] == number[length - 1])
+        {
+            return true;
+        }
+    return false;
+}
+
+if (IsPalindrome()) Console.WriteLine($"{number} палиндром");
+else Console.WriteLine($"{number} не палиндром");
+
