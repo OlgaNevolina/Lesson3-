3e1f1da05ebcfc7fb7897f6e7ecec1bbdd31ccd3
nevol78@mail.ru <nevol78@mail.ru>
2022-05-20 19:15:37 +0300

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите целое число ");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+
+while(i < N + 1)
+{
+    Console.WriteLine($"Число {i , 5} | Куб числа {i * i * i , 5}");
+    i ++;
+}
+
