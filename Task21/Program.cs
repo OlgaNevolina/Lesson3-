ae79391595d29f46ad92f888c2bad4527660da30
nevol78@mail.ru <nevol78@mail.ru>
2022-05-20 19:09:55 +0300

    Task21

---
+++ Task21/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите координаты первой точки ");
+double x1 = double.Parse(Console.ReadLine());
+double y1 = double.Parse(Console.ReadLine());
+double z1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки ");
+double x2 = double.Parse(Console.ReadLine());
+double y2 = double.Parse(Console.ReadLine());
+double z2 = double.Parse(Console.ReadLine());
+
+double sum = (x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2) + (z1 - z2)*(z1 - z2);
+double result = Math.Sqrt(sum);
+double n = Math.Round(result, 2);
+
+Console.WriteLine($"Раcстояние между точками {n}");
